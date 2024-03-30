24b990565cc6486e6e993745fef88b01b13e3edb
maiduyenjune <146368907+maiduyenjune@users.noreply.github.com>
2024-03-30 17:52:11 -0400

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,18 +4,19 @@ import { css } from "@emotion/react";
 import routeLinks from "@/routeLinks";
 import { AppContext } from "@/context/AppContext";
 import logoIcon from "@/assets/img/icon-layout.png";
-import Image from "next/image";
 
 const logo = css`
   font-size: 2rem;
   font-weight: 700;
   transition: all 0.3s ease;
+  height: 40px;
+  width:40px;
 `;
 
 const Logo: FC = () => {
   return (
-    <Link href={routeLinks.homePage()} css={logo}>
-      <Image src={logoIcon.src} height={40} width={40} alt="June" />
+    <Link href={routeLinks.homePage()}>
+      <img src={logoIcon.src} css={logo} alt="June" />
     </Link>
   );
 };
